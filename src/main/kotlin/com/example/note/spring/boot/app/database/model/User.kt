8609fc70c8e1668af1836d1ed9860b2b621c7e33
m0bileDev <damian.ogorek.1996@gmail.com>
2025-12-03 22:12:09 +0100

    refactor(model): add annotation
    Changed property name

--- src/main/kotlin/com/example/note/spring/boot/app/database/model/User.kt
+++ src/main/kotlin/com/example/note/spring/boot/app/database/model/User.kt
@@ -2,10 +2,12 @@ package com.example.note.spring.boot.app.database.model
 
 import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
 
+@Document("users")
 data class User(
     val email: String,
-    val password: String,
+    val hashedPassword: String,
     //mongodb primary auto-increment id
     @Id val id: ObjectId = ObjectId()
 )
