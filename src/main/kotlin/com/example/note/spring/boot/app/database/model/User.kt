7d5adc9762b9c0635bd1d75a3d67bda6f29d699e
m0bileDev <damian.ogorek.1996@gmail.com>
2025-12-03 22:33:38 +0100

    refactor(model): updated type of password property

--- src/main/kotlin/com/example/note/spring/boot/app/database/model/User.kt
+++ src/main/kotlin/com/example/note/spring/boot/app/database/model/User.kt
@@ -1,5 +1,6 @@
 package com.example.note.spring.boot.app.database.model
 
+import com.example.note.spring.boot.app.security.Password
 import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
@@ -7,7 +8,7 @@ import org.springframework.data.mongodb.core.mapping.Document
 @Document("users")
 data class User(
     val email: String,
-    val hashedPassword: String,
+    val hashedPassword: Password,
     //mongodb primary auto-increment id
     @Id val id: ObjectId = ObjectId()
 )
