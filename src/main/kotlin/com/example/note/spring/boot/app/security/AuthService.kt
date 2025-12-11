4c8141c18f2685c490344331b7c5071ce9bd8d31
m0bileDev <damian.ogorek.1996@gmail.com>
2025-12-11 23:42:01 +0100

    feature(security): provide function which hash refresh token

--- src/main/kotlin/com/example/note/spring/boot/app/security/AuthService.kt
+++ src/main/kotlin/com/example/note/spring/boot/app/security/AuthService.kt
@@ -1,9 +1,12 @@
 package com.example.note.spring.boot.app.security
 
 import com.example.note.spring.boot.app.database.model.User
+import com.example.note.spring.boot.app.database.repository.RefreshTokenRepository
 import com.example.note.spring.boot.app.database.repository.UserRepository
 import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.stereotype.Service
+import java.security.MessageDigest
+import java.util.*
 import javax.security.auth.login.CredentialException
 
 typealias Email = String
@@ -17,7 +20,8 @@ data class TokenPair(
 class AuthService(
     private val jwtService: JwtService,
     private val userRepository: UserRepository,
-    private val hashEncoder: HashEncoder
+    private val hashEncoder: HashEncoder,
+    private val refreshTokenRepository: RefreshTokenRepository
 ) {
     fun register(email: Email, password: Password): User {
         return userRepository.save(
@@ -42,5 +46,11 @@ class AuthService(
             accessToken = newAccessToken,
             refreshToken = newRefreshToken
         )
+    }
+
+    private fun hashToken(token: Token): Token {
+        val digest = MessageDigest.getInstance("SHA-256")
+        val hashBytes = digest.digest(token.encodeToByteArray())
+        return Base64.getEncoder().encodeToString(hashBytes)
     }
 }
