c784b209021c94480fb7e6822e96b47f727f11dd
m0bileDev <damian.ogorek.1996@gmail.com>
2025-12-11 23:30:10 +0100

    feature(model): provide model for refresh token collections

---
+++ src/main/kotlin/com/example/note/spring/boot/app/database/model/RefreshToken.kt
@@ -0,0 +1,19 @@
+package com.example.note.spring.boot.app.database.model
+
+import com.example.note.spring.boot.app.controllers.OwnerId
+import com.example.note.spring.boot.app.security.Token
+import org.bson.types.ObjectId
+import org.springframework.data.mongodb.core.index.Indexed
+import org.springframework.data.mongodb.core.mapping.Document
+import java.time.Instant
+
+//mongodb annotation this model is a document type -> refresh token collection
+@Document("refresh_tokens")
+data class RefreshToken(
+    val userId: ObjectId,
+    val hashedToken: Token,
+//    mongo atlas db will periodically check expiresAt property and remove it from db when conditions are met
+    @Indexed(expireAfter = "0s")
+    val expiresAt: Instant,
+    val createdAt: Instant = Instant.now()
+)
