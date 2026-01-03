2f9306b68dc64967fdd482a219aeb1209d7a2cc2
m0bileDev <damian.ogorek.1996@gmail.com>
2026-01-03 14:37:59 +0100

    refactor(security): provide additional logic to security config
    Configured which endpoints need authorization and which not, provide more descriptive errors and provide 401 http status code instead of 403 for authorization related errors.

--- src/main/kotlin/com/example/note/spring/boot/app/security/SecurityConfig.kt
+++ src/main/kotlin/com/example/note/spring/boot/app/security/SecurityConfig.kt
@@ -1,10 +1,13 @@
 package com.example.note.spring.boot.app.security
 
+import jakarta.servlet.DispatcherType
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.HttpStatusEntryPoint
 
 // use this class to configure some parts of the app
 @Configuration
@@ -18,6 +21,26 @@ class SecurityConfig {
         return httpSecurity
             .csrf { csrf -> csrf.disable() }
             .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            .authorizeHttpRequests { auth ->
+                auth
+                    //permit all endpoints under auth that do not need authorization headers
+                    .requestMatchers("/auth/**")
+                    .permitAll()
+                    //until dispatcher is set SpringBoot maps all error to 403, but in case more
+                    //fine-grained error are needed dispatcher has to be added
+                    .dispatcherTypeMatchers(
+                        DispatcherType.ERROR,
+                        DispatcherType.FORWARD
+                    )
+                    .permitAll()
+                    //after the rules from above any request has to be authenticated
+                    .anyRequest()
+                    .authenticated()
+            }
+            .exceptionHandling { configure ->
+                //if authentication fails, by default http status code will be 401
+                configure.authenticationEntryPoint(HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+            }
             .build()
     }
 }
