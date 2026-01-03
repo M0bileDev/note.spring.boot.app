7ffd8ad8b56327f0aeb3648850df5afd1e740ead
m0bileDev <damian.ogorek.1996@gmail.com>
2026-01-03 15:13:20 +0100

    docs(model): provide more information about config process

--- src/main/kotlin/com/example/note/spring/boot/app/database/model/RefreshToken.kt
+++ src/main/kotlin/com/example/note/spring/boot/app/database/model/RefreshToken.kt
@@ -1,6 +1,5 @@
 package com.example.note.spring.boot.app.database.model
 
-import com.example.note.spring.boot.app.controllers.OwnerId
 import com.example.note.spring.boot.app.security.Token
 import org.bson.types.ObjectId
 import org.springframework.data.mongodb.core.index.Indexed
@@ -13,6 +12,7 @@ data class RefreshToken(
     val userId: ObjectId,
     val hashedToken: Token,
 //    mongo atlas db will periodically check expiresAt property and remove it from db when conditions are met
+//    this will not be created automatically, need to update properties config -> spring.data.mongodb.auto-index-creation=true
     @Indexed(expireAfter = "0s")
     val expiresAt: Instant,
     val createdAt: Instant = Instant.now()
